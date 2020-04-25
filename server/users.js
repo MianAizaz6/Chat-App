273b6263dbdda748f452e6885fbef7bf053dac56
MianAizaz6 <48237975+MianAizaz6@users.noreply.github.com>
2020-04-26 04:51:50 +0500

    Completed

--- server/users.js
+++ server/users.js
@@ -1,28 +1,29 @@
- users= [];
+const users = [];
 
- addUser = ({ id, name, room } ) => {
+const addUser = ({ id, name, room }) => {
   name = name.trim().toLowerCase();
   room = room.trim().toLowerCase();
 
-  const exitingUsers = users.find((user)=> user.room === room && user.name === name );
+  const existingUser = users.find((user) => user.room === room && user.name === name);
 
-  if(exitingUsers){
-    return {error : 'Sorry UserName Exists..'}
-  }
-  const user = {id , name, room};
+  if(!name || !room) return { error: 'Username and room are required.' };
+  if(existingUser) return { error: 'Username is taken.' };
+
+  const user = { id, name, room };
+
   users.push(user);
-  return { user}
+
+  return { user };
 }
 
- removeUser = () => {
-  const index = users.findIndex((user)=> user.id === id );
-   if(index !== -1){
-     return users.splice(index,1)[0];
-   }
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+
+  if(index !== -1) return users.splice(index, 1)[0];
 }
 
- getUser = (id) => users.find((user)=> user.id === id);
+const getUser = (id) => users.find((user) => user.id === id);
 
-  getUserInRoom = (room) => user.filter((user)=> user.room === room);
-  
-  module.exports = { addUser, getUser , removeUser , getUserInRoom};
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
